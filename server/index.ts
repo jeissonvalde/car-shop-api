ec9d3cf6f6deb12641c762f16e3157f0f7b20877
Jeisson Valderrama <jeandvm1023@gmail.com>
2022-12-10 14:48:44 -0500

    Handle as middleware for error in reqs.

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,23 @@
 // Server API
 import express from 'express'
+import config from './config/server.json'
+import bodyParser from 'body-parser'
 
-const app = express(),
-  config = {
-    port: 5000
-  }
+const app = express()
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/cars', (_req, res) => {
   res.json({ cars: ['Renault', 'Chevrolet', 'Toyota'] })
+})
+
+app.use((err, _req, res, _next) => {
+  if (err.message.match(/not found/)) {
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message })
 })
 
 app.listen(config.port, () => {
