a1184de356c2633cd16c2f43796f8c6fb139a6d7
Jeisson Valderrama <jeandvm1023@gmail.com>
2022-12-27 13:45:25 -0500

    First types.

---
+++ server/types.d.ts
@@ -0,0 +1,11 @@
+// Main car type.
+export interface PrivateCarEntry {
+  id: string,
+  name: string,
+  lastname: string,
+}
+
+export interface Secret {
+  secret: string,
+  algorithms: string[]
+}
